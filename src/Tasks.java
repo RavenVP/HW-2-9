f00d2df086cdbd1bab356c6bd3e7b49c73466bf9
RavenVP <Raven_vp_72@mail.ru>
2022-11-08 00:17:40 +0500

    Доработал HW 2-9(1,2) v3

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,15 +1,19 @@
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class Tasks {
 
     public static void main(String[] args) {
-        Set<Integer> numbers = new HashSet<>();
-        int r;
+        List<Integer> numbers = new LinkedList<>();
         for (int i = 0; i < 20; i++) {
-            numbers.add((int)Math.round(Math.random()*1000));
+            if (numbers.size() < 20) {
+                numbers.add((int) Math.round(Math.random() * 30));
+            }
         }
         numbers.removeIf(integer -> integer % 2 != 0);
         System.out.println(numbers);
+
     }
 }
