a36b255cf737eeebc5e0f723496913ebe8188d40
RavenVP <Raven_vp_72@mail.ru>
2022-11-13 16:48:53 +0500

    Доработал HW 2-9(1,2) v4

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,21 +1,21 @@
 import Market.Product;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Recipe {
     private final String recipeName;
-    private int totalCost = 0;
-    private Set<Product> products = new HashSet<Product>();
+    private int totalCost;
+    private final HashMap<Product, Integer> products = new HashMap<Product, Integer>();
 
     public Recipe(String name) {
         this.recipeName = name;
+        this.totalCost = 0;
+
     }
 
      public void addProductInRecipe(Product product, int quantityOfProducts) {
-        if (!products.contains(product)) {
-            products.add(product);
+        if (!products.containsKey(product)) {
+            products.put(product,quantityOfProducts);
         } else {
             throw new RuntimeException("Уже есть");
         }
@@ -23,23 +23,22 @@ public class Recipe {
     }
 
 
-
-
-     public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Recipe recipes = (Recipe) o;
-        return Objects.equals(recipeName, recipes.recipeName);
+        Recipe recipe = (Recipe) o;
+        return totalCost == recipe.totalCost && Objects.equals(recipeName, recipe.recipeName) && Objects.equals(products, recipe.products);
     }
 
     @Override
-     public int hashCode() {
-        return Objects.hash(recipeName);
+    public int hashCode() {
+        return Objects.hash(recipeName, totalCost, products);
     }
 
     public String getRecipeName() {return recipeName;}
     public int getTotalCost() {return totalCost;}
-    public Set<Product> getProducts() {return products;}
+    public HashMap<Product,Integer> getProducts() {return products;}
 
     @Override
     public String toString() {
