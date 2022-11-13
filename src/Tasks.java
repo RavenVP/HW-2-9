bbd7e192dd20d05db1ef5c35876ed47e6e7c569e
RavenVP <Raven_vp_72@mail.ru>
2022-11-13 16:50:38 +0500

    Доработал HW 2-9(1,2) v4

--- src/Tasks.java
+++ src/Tasks.java
@@ -7,13 +7,16 @@ public class Tasks {
 
     public static void main(String[] args) {
         List<Integer> numbers = new LinkedList<>();
-        for (int i = 0; i < 20; i++) {
-            if (numbers.size() < 20) {
-                numbers.add((int) Math.round(Math.random() * 30));
+            for (int i = 0; i < 20; i++) {
+                int random = (int) (Math.random() * 1000);
+                numbers.add(random);
             }
+            numbers.removeIf(integer -> integer %2 != 0);
+            System.out.println(numbers);
+
         }
-        numbers.removeIf(integer -> integer % 2 != 0);
-        System.out.println(numbers);
 
     }
-}
+
+
+
