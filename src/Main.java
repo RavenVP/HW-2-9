b68ce8816ecb441138b4fc98b059a8a8ee003524
RavenVP <Raven_vp_72@mail.ru>
2022-11-14 19:22:22 +0500

    Выполнил HW 10-2

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,30 @@ import Market.ProductList;
 
 public class Main {
     public static void main(String[] args) {
-        Product cucumber = new Product("Огрурец", 13, 1.5);
+        Product cucumber = new Product("Огрурец", 15, 1);
         Product tomato = new Product("Томат", 10, 1);
-        Product grape = new Product("Виноград", 20, 2);
+        Product grape = new Product("Виноград", 20, 1);
 
-        ProductList.addProductToList(cucumber);
-        ProductList.addProductToList(tomato);
-        ProductList.addProductToList(grape);
-        ProductList.removeProductFromList(tomato);
-        ProductList.checkProductInList(tomato);
-        ProductList.printProductList();
+//        ProductList.addProductToList(cucumber);
+//        ProductList.addProductToList(tomato);
+//        ProductList.addProductToList(grape);
+//        ProductList.removeProductFromList(tomato);
+//        ProductList.checkProductInList(tomato);
+//        ProductList.printProductList();
 
         Recipe cake = new Recipe("Торт");
-        cake.addProductInRecipe(grape, 4);
-        cake.addProductInRecipe(cucumber, 6);
+        cake.addProductInRecipe(grape,10);
 
         Recipe salad = new Recipe("Салат");
-        salad.addProductInRecipe(tomato, 4);
-        salad.addProductInRecipe(cucumber, 6);
+        salad.addProductInRecipe(cucumber, 10);
+        salad.addProductInRecipe(tomato,10);
 
         BookOfRecipe.addRecipe(salad);
         BookOfRecipe.addRecipe(cake);
 
+        System.out.println(salad);
+        System.out.println(cake);
+
         BookOfRecipe.printBookOfRecipe();
-
-
     }
 }
--- src/Recipe.java
+++ src/Recipe.java
@@ -4,13 +4,10 @@ import java.util.*;
 
 public class Recipe {
     private final String recipeName;
-    private int totalCost;
     private final HashMap<Product, Integer> products = new HashMap<Product, Integer>();
 
     public Recipe(String name) {
         this.recipeName = name;
-        this.totalCost = 0;
-
     }
 
      public void addProductInRecipe(Product product, int quantityOfProducts) {
@@ -19,7 +16,30 @@ public class Recipe {
         } else {
             throw new RuntimeException("Уже есть");
         }
-        totalCost += quantityOfProducts * product.getPrice();
+    }
+
+    public int totalRecipeCost() {
+        int sum = 0;
+        for (Product product : products.keySet()) {
+            sum += products.get(product) * product.getPrice();
+        }
+        return sum;
+    }
+
+
+
+
+    public String getRecipeName() {return recipeName;}
+    public int getTotalCost() {return totalRecipeCost();}
+    public HashMap<Product,Integer> getProducts() {return products;}
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "recipeName='" + getRecipeName() + '\'' +
+                ", totalCost=" + getTotalCost() +
+                ", products=" + getProducts() +
+                '}';
     }
 
 
@@ -28,25 +48,11 @@ public class Recipe {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Recipe recipe = (Recipe) o;
-        return totalCost == recipe.totalCost && Objects.equals(recipeName, recipe.recipeName) && Objects.equals(products, recipe.products);
+        return Objects.equals(recipeName, recipe.recipeName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(recipeName, totalCost, products);
-    }
-
-    public String getRecipeName() {return recipeName;}
-    public int getTotalCost() {return totalCost;}
-    public HashMap<Product,Integer> getProducts() {return products;}
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "recipeName='" + recipeName + '\'' +
-                ", totalCost=" + totalCost +
-                ", products=" + products +
-                ", quantityOfProducts=" +
-                '}';
+        return Objects.hash(recipeName);
     }
 }
